c8b06886fa8bb1be146c5c3467936b2a53d21e97
Akash Adak <161819740+Akash-Adak@users.noreply.github.com>
2025-08-26 18:03:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if a String is Subsequence of Other/check-if-a-string-is-subsequence-of-other.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubSeq(String s1, String s2) {
+        int i = 0, j = 0;
+        while (i < s2.length() && j < s1.length()) {
+            if (s2.charAt(i) == s1.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == s1.length();
+    }
+}
