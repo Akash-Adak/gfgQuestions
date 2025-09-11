427752816e2b93ab0c318eeba54213c7240bf6b8
Akash Adak <161819740+Akash-Adak@users.noreply.github.com>
2025-09-11 10:28:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        if (arr.length <= 1) return 0;
+        if (arr[0] == 0) return -1;
+
+        int maxReach = arr[0]; // The farthest index we can reach
+        int steps = arr[0];    // Steps we can still take
+        int jumps = 1;         // Number of jumps made
+
+        for (int i = 1; i < arr.length; i++) {
+            if (i == arr.length - 1) return jumps;
+
+            maxReach = Math.max(maxReach, i + arr[i]);
+            steps--;
+
+            if (steps == 0) {
+                jumps++;
+                if (i >= maxReach) return -1;
+                steps = maxReach - i;
+            }
+        }
+
+        return -1;
+    }
+}
